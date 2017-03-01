0169f1363c71b207ca4d387015606a165cfbf261
CCrowRun <carrie.g.crow@gmail.com>
2017-03-01 10:47:15 -0600

    Refined Design
    Removed text, simplified gradient

--- app.js
+++ app.js
@@ -4,20 +4,21 @@
 var app = angular.module('checklist', []);
 
 app.controller('ItemController', function(){
+//Used for "ng-submit" directive to add items to array.
 	this.list = items;
 	this.newItem = {};
 	this.addItem = function (){
 		this.list.push(this.newItem);
 		this.newItem = {};
 	};
-
+//Used in "ng-click" directive for Delete button
 	this.remove=function($index){ 
  	 this.list.splice($index,1);     
 	};
 
 });
 
-
+//Array of checklist items
 var items = [
 {
 	body: "Create checklist"
